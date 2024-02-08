8b9588ad93027e7a9fe852fa30b0d49903df65f4
Supriya Agrawal <supriyaagrawal1998@gmail.com>
2024-02-08 15:49:08 +0530

    Added ts

--- src/componets/button/index.tsx
+++ src/componets/button/index.tsx
@@ -1,11 +1,21 @@
-import { ButtonStyled } from "./style";
-
-const Button = ({btnText, buttonClass, disabled, secondBtn}) => {
-    return (
-        <ButtonStyled className={`${buttonClass}`} disabled={disabled} secondBtn={secondBtn}>
-            {btnText}
-        </ButtonStyled>
-    )
+import { ButtonStyled } from './style';
+interface Props {
+  btnText: string;
+  buttonClass: string;
+  disabled?: boolean;
+  secondBtn?: any;
 }
+const Button = (props: Props) => {
+  const { btnText, buttonClass, disabled, secondBtn } = props;
+  return (
+    <ButtonStyled
+      className={`${buttonClass}`}
+      disabled={disabled}
+      secondBtn={secondBtn}
+    >
+      {btnText}
+    </ButtonStyled>
+  );
+};
 
 export default Button;
